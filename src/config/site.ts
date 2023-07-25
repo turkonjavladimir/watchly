9b30c293c0b0b1320940b554d6e26c918f4d604d
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-25 17:58:16 +0200

    chore: add metadata

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Watchly",
+  description:
+    "Watchly is your go-to entertainment app to discover and bookmark your favorite movies and TV shows",
+  url: "https://watchly.wyattturkonja.com/",
+  links: {
+    github: "https://github.com/turkonjavladimir/watchly",
+  },
+}
