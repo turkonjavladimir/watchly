f7aae4afad77c0c1fcd3cde5db990aa6ab5d8d1e
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-26 23:39:10 +0200

    chore: define themes

---
+++ src/config/theme.ts
@@ -0,0 +1,19 @@
+import { Theme } from "@/types"
+
+export const themeOptions: Theme[] = [
+  {
+    value: "light",
+    label: "Light",
+    icon: "sun",
+  },
+  {
+    value: "dark",
+    label: "Dark",
+    icon: "moon",
+  },
+  {
+    value: "system",
+    label: "System",
+    icon: "monitor",
+  },
+]
